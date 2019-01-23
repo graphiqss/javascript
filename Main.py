296bdea9d3a7575d437a6fe09ddc91afd25b896d
graphiqss <46732130+graphiqss@users.noreply.github.com>
2019-01-23 12:02:40 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,5 +3,14 @@ from discord.ext import commands
 
 @bot.event
 async def on_ready():
-
+  print("Logged in as")
+  print(bot.user.name)
+  print(bot.user.id)
+  
+@bot.command(pass_context=True)
+async def leave(ctx, server: id):
+  server = ctx.message.server.id
+  await bot.get_server(server)
+  await bot.leave(server)
+  
 bot.run(os.environ['BOT_TOKEN'])
