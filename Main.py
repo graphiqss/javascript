f662ccd9330668a1076f1fd740649735450f744f
graphiqss <46732130+graphiqss@users.noreply.github.com>
2019-01-23 12:22:11 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def leave(ctx, server: id):
   server = ctx.message.server.id
-  await bot.get_server(server)
+  bot.get_server(server)
   await bot.leave(server)
   
 bot.run(os.environ['BOT_TOKEN'])
